0a5385959719d2d55cbdf6eb826641193458f3de
张成智 <183472491@qq.com>
2016-05-31 14:32:05 +0800

    添加功能代码

---
+++ Assets/SpriteFade.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpriteFade : MonoBehaviour
+{
+
+    public float Duration = 2.0f;
+    float elapse;
+    // Use this for initialization
+    void Start()
+    {
+        SetAlpha(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (elapse < Duration)
+        {
+            elapse += Time.deltaTime;
+            SetAlpha(elapse / Duration);
+        }
+    }
+
+    void SetAlpha(float a)
+    {
+        var sp = GetComponent<SpriteRenderer>();
+        var c = sp.color;
+        c.a = a;
+        sp.color = c;
+    }
+}
---
+++ Assets/StoryBoard.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoryBoard : MonoBehaviour
+{
+
+    public GameObject particle;
+    public GameObject face;
+    public GameObject bat;
+    public GameObject word;
+
+    float[] timeArray = new float[] { 4, 6, 8, 10, 12 };
+
+    int index;
+    bool canClick;
+
+    // Use this for initialization
+    void Start()
+    {
+        particle.SetActive(false);
+        face.SetActive(false);
+        bat.SetActive(false);
+        word.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //realtimeSinceStartup 开始到目前的时间秒数
+        if (index < timeArray.Length && Time.realtimeSinceStartup > timeArray[index])
+        {
+            OnTime(index);
+            index++;
+        }
+        if (canClick && Input.GetMouseButtonUp(0))
+        {
+            word.SetActive(true);
+        }
+    }
+
+    private void OnTime(int index)
+    {
+        switch (index)
+        {
+            case 0:
+                particle.SetActive(true);
+                break;
+            case 1:
+                bat.SetActive(true);
+                break;
+            case 2:
+                face.SetActive(true);
+                break;
+            case 3:
+                face.GetComponent<AudioSource>().Play();
+                break;
+            case 4:
+                canClick = true;
+                break;
+            default:
+                break;
+        }
+    }
+}
